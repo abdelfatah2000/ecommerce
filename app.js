d5341e72795127668f9dea1accb7d9613d67a3f9
abdelfatah2000 <abdelfattahmohamed596@gmail.com>
2022-01-02 02:19:11 +0200

    more and more edit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+
 require("dotenv").config();
+
+
 const connection = require("./config/db");
 connection();
 const PORT = process.env.PORT || 3000
 
+app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 

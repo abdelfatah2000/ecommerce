84b16972e7bd6d6499c7aeb02c97fc3c29193541
abdelfatah2000 <abdelfattahmohamed596@gmail.com>
2022-01-05 18:03:14 +0200

    code errorrs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
 
 require("dotenv").config();
@@ -10,9 +9,8 @@ const connection = require("./config/db");
 connection();
 const PORT = process.env.PORT || 3000
 
-app.use(cors());
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 const users = require("./modules/routes/user.routes");
 const products = require("./modules/routes/product.routes");
@@ -20,12 +18,15 @@ const categories = require("./modules/routes/category.routes");
 const carts = require("./modules/routes/cart.routes");
 const orders = require("./modules/routes/order.routes");
 const wishlist = require("./modules/routes/wishlist.routes");
+const admin = require("./modules/routes/admin.routes");
 app.use("/api/users", users);
 app.use("/api/products", products);
 app.use("/api/category", categories);
 app.use("/api/carts", carts);
 app.use("/api/orders", orders);
 app.use("/api/wishlist", wishlist);
+app.use("/admin", admin);
+
 
 app.use("/uploads", express.static("uploads"));
 
